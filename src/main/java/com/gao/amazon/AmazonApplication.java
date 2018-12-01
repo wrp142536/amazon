096f6cb4dbffeb90c6742fa2bd86a0fa80383f76
zhangkai <zhangkai@qq.com>
2018-12-01 20:47:33 +0800

    冲突解决

--- src/main/java/com/gao/amazon/AmazonApplication.java
+++ src/main/java/com/gao/amazon/AmazonApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AmazonApplication {
 
 	public static void main(String[] args) {
+		System.out.println("AmazonApplication.main()");
 		SpringApplication.run(AmazonApplication.class, args);
 	}
 }

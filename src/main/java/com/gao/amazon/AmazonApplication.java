b633c22ac30c5ea9b94c549696dd29e4ac1d5541
eclipse_zk <eclipse_zk@qq.com>
2018-12-01 20:14:14 +0800

    初始化controller包
    Change-Id: I9c70f25d53d8db2aa30c5a009b9b9310525e39c0
    Signed-off-by: eclipse_zk <eclipse_zk@qq.com>

---
+++ src/main/java/com/gao/amazon/AmazonApplication.java
@@ -0,0 +1,12 @@
+package com.gao.amazon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmazonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmazonApplication.class, args);
+	}
+}
---
+++ src/main/java/com/gao/amazon/controller/UploadFileController.java
@@ -0,0 +1,5 @@
+package com.gao.amazon.controller;
+
+public class UploadFileController {
+
+}
